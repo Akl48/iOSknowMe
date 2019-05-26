ee1b60f3c5a86dfbd1b94b2d065c4de6cb927ab8
Anthony_ <1003180548@qq.com>
2019-05-26 10:24:46 +0800

    AI&Main&Mine

---
+++ 知己/Root/Me/MineViewController.m
@@ -0,0 +1,32 @@
+//
+//  MineViewController.m
+//  知己
+//
+//  Created by Anthony Zhang on 2019/5/26.
+//  Copyright © 2019 周天荣. All rights reserved.
+//
+
+#import "MineViewController.h"
+
+@interface MineViewController ()
+
+@end
+
+@implementation MineViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
--- 知己/Root/ZTRViewController.m
+++ 知己/Root/ZTRViewController.m
@@ -7,6 +7,10 @@
 //
 
 #import "ZTRViewController.h"
+#import "Main/MainViewController.h"
+#import "AI/AIViewController.h"
+#import "Me/MineViewController.h"
+
 
 @interface ZTRViewController ()
 
@@ -17,7 +21,23 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor whiteColor];
+    [self loadAllControllers];
+}
+
+- (void)loadAllControllers {
+    MainViewController *main = [[MainViewController alloc]init];
+    [self addChildViewController:main];
+    main.tabBarItem = [[UITabBarItem alloc]initWithTitle:@"首页" image:[UIImage imageNamed:@"首页"] selectedImage:[UIImage imageNamed:@"首页"]];
     
+    
+    AIViewController *AI = [[AIViewController alloc]init];
+    [self addChildViewController:AI];
+    AI.tabBarItem = [[UITabBarItem alloc]initWithTitle:@"问答" image:[UIImage  imageNamed:@"问答"] selectedImage:[UIImage imageNamed:@"问答"]];
+    
+    
+    MineViewController *mine = [[MineViewController alloc]init];
+    [self addChildViewController:mine];
+    mine.tabBarItem = [[UITabBarItem alloc]initWithTitle:@"用户" image:[UIImage imageNamed:@"用户"] selectedImage:[UIImage imageNamed:@"用户"]];
 }
 
 
